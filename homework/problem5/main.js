13eae01c3a30d2a8a1479331eb161da6dd836152
Dimo Chakarov <jackckiller@gmail.com>
2016-08-27 19:52:37 +0300

    Hope this works
    I hope wthis works

---
+++ homework/problem5/main.js
@@ -0,0 +1,75 @@
+//piece of code that does one or more actions
+// dry do not repeat yourself 
+//function go (name, age){
+//    alert(name);
+//    alert(age);
+//    
+//}
+//go('will', '34');
+//go('laura', '28');
+//
+//function go (name, age){
+//   if(age < 20){
+//       return name + '!';
+//   }else{
+//       return name;
+//   }
+//}
+//
+//alert(go('will', 34));
+
+//function go () {
+//    alert('hi');
+//}
+//var mylist=[ 'apples', 'oranges', 'bananas'];
+//mylist.forEach(function(value,index){
+// alert('i have'+value+'in my shoping bag')       
+//               });
+//               
+
+//while do for
+var times = 0;
+
+//while(times < 10){
+//    console.log('tried it', times);
+//    times = times++;
+//    
+//}
+// 
+//
+//do{
+//    console.log('tried it ', times);
+//    times++;
+//}while(times <100);
+//
+//
+//
+
+//for (setup, comparison, change){}
+//for (var i=0; i<10; i++){}
+//
+//functions
+//function go(name, age){
+//    alert('name');
+//    alert('age');
+//}
+//go('will' 34);
+//go('Layra' 28);
+//
+
+//function go(name, age){
+//if(age<20){
+//    return name + '!';
+//}else{
+//    return name;
+//}
+//}
+//
+//alert(go('will', 19));
+//
+//               
+//               
+       //array loops
+var myList=['apples','oanges','left','right','soup'];
+               
+               
